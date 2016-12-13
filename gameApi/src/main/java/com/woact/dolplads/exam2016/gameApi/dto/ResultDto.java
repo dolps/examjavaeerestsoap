a5e04497b8a5e69a1bb9743df87100aa8c661126
dolps <dolplads@gmail.com>
2016-12-13 01:15:27 +0100

    everything builds

---
+++ gameApi/src/main/java/com/woact/dolplads/exam2016/gameApi/dto/ResultDto.java
@@ -0,0 +1,7 @@
+package com.woact.dolplads.exam2016.gameApi.dto;
+
+/**
+ * Created by dolplads on 12/12/2016.
+ */
+public class ResultDto {
+}
---
+++ gameApi/src/test/java/com/woact/dolplads/exam2016/gameApi/client/GameApplicationTestBase.java
@@ -0,0 +1,7 @@
+package com.woact.dolplads.exam2016.gameApi.client;
+
+/**
+ * Created by dolplads on 13/12/2016.
+ */
+public class GameApplicationTestBase {
+}

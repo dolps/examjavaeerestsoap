621a128f9bfb35ea5bb96cd4ea91e5c743926bb6
dolps <dolplads@gmail.com>
2016-12-13 09:24:27 +0100

    refactored dw

--- gameApi/src/main/java/com/woact/dolplads/exam2016/gameApi/core/Quiz.java
+++
@@ -1,7 +0,0 @@
-package com.woact.dolplads.exam2016.gameApi.core;
-
-/**
- * Created by dolplads on 29/11/2016.
- */
-public class Quiz {
-}
--- gameApi/src/test/java/com/woact/dolplads/exam2016/gameApi/client/GameResourceTest.java
+++ gameApi/src/test/java/com/woact/dolplads/exam2016/gameApi/client/GameResourceTest.java
@@ -2,64 +2,39 @@ package com.woact.dolplads.exam2016.gameApi.client;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.google.gson.Gson;
+import com.woact.dolplads.exam2016.dtos.dto.QuizDto;
 import com.woact.dolplads.exam2016.gameApi.GameApplication;
 import com.woact.dolplads.exam2016.gameApi.GameConfiguration;
+import com.woact.dolplads.exam2016.gameApi.dto.AnswerDto;
+import com.woact.dolplads.exam2016.gameApi.dto.GameDto;
+import com.woact.dolplads.exam2016.gameApi.dto.ResultDto;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.junit.*;
 
 import static io.restassured.RestAssured.*;
-import static junit.framework.TestCase.fail;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Is.is;
 
-import io.restassured.RestAssured;
 import io.restassured.RestAssured;
 import io.restassured.http.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by dolplads on 01/12/2016.
  */
-public class GameResourceTest {
+public class GameResourceTestNot extends GameApplicationTestBase {
     @ClassRule
     public static final DropwizardAppRule<GameConfiguration> RULE =
             new DropwizardAppRule<>(GameApplication.class, ResourceHelpers.resourceFilePath("game.yml"));
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(Integer.parseInt(RULE.getConfiguration().getTestUrl()));
-
-    @BeforeClass
-    public static void setupClass() {
-        System.out.println("halla" + RULE.getConfiguration().getTestUrl());
-
-        RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 9000;
-        RestAssured.basePath = "/app/api/games";
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-    }
-
-    @Test
-    public void hello() throws Exception {
-        stubRandomQuizzes("1L");
-        //String res = when().request(Method.GET).then().assertThat().extract().as(String.class);
-        //System.out.println(res);
-        String hello = given().accept(ContentType.TEXT).when().get("/hei").then().statusCode(200).extract().asString();
-        assertEquals(hello, "helloi");
-    }
-
-
-    // stubs the endpoint quiz/api/randomQuizzes
-    private void stubRandomQuizzes(String json) {
-        Long x = 1L;
-        stubFor(post(urlEqualTo("/quiz/api/categories"))
-                .withHeader("Content-Type", WireMock.equalTo("application/json"))
-                //.withRequestBody(WireMock.equalTo(toJson))
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(json)
-                        .withStatus(200)));
-
-    }
-
 }
